3d50a23d408ea943b392b13de638509bba3b4ac6
Philipi Magalhães <philipimagalhaes@gmail.com>
2020-07-21 22:51:06 -0300

    updated code

---
+++ HttpTrigger1/index.js
@@ -0,0 +1,27 @@
+const Crypto = require('crypto');
+module.exports = async function (context, req) {
+    const key = 'QpcTkEczVcZF/voqotpJCuwsNnzBF1baq2LbcwtndQOjkGfa2Am4UA==';
+    const hmac = Crypto.createHmac('sha1', key);
+    const signature = hmac.update(JSON.stringify(req.body)).digest('hex');
+    const shaSignature = `sha1=${signature}`;
+    const gitHubSignature = req.headers['x-hub-signature'];
+    if (!shaSignature.localeCompare(gitHubSignature)) {
+        if (req.body.pages[0].title) {
+            context.res = {
+                body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: ("Invalid payload for Wiki event")
+            }
+        }
+    }
+    else {
+    context.res = {
+        status: 401,
+        body: "Signatures don't match"
+    };
+}
+}
